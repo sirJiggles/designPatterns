1a2ebcdc8528c097e9b6803e3e3eb735fb33adc0
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-09-02 17:11:57 +0200

    some more messing arround with it :D

--- strategy/run.ts
+++ strategy/run.ts
@@ -3,15 +3,26 @@
 
 import RubberDuck from './Ducks/RubberDuck';
 import RegularDuck from './Ducks/RegularDuck';
+import FlyWithWings from './FlyBehavior/FlyWithWings';
 
 console.log('Test rubber duck:');
+console.log('');
 const rubberDuck = new RubberDuck();
 
 console.log('can you fly?');
 rubberDuck.performFly();
 console.log('can you quack?');
 rubberDuck.performQuack();
+console.log('');
 
+console.log('lets make you fly my little rubber duck');
+// this is run time modification of what would have been a concrete class using inheritance
+rubberDuck.flyBehavior = new FlyWithWings();
+console.log('NOW can you fly?');
+rubberDuck.performFly();
+
+console.log('');
+console.log('');
 console.log('Test regular duck:');
 const regularDuck = new RegularDuck();
 
